4a20377868c49bc0cc612b1d80cbcc21eea87476
“Kevin <logikevcoder@gmail.com>
2017-10-11 18:34:08 -0500

    Remade the cardClickListener function to make the function work as intended.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 let cards = [
-        "fa fa-diamond",
-        "fa fa-leaf",
-        "fa fa-cube",
-        "fa fa-bomb",
-        "fa fa-bicycle",
-        "fa fa-anchor",
-        "fa fa-paper-plane-o",
-        "fa fa-bolt",
-        "fa fa-diamond",
-        "fa fa-leaf",
-        "fa fa-cube",
-        "fa fa-bomb",
-        "fa fa-bicycle",
-        "fa fa-anchor",
-        "fa fa-paper-plane-o",
-        "fa fa-bolt"
+        "diamond",
+        "diamond",
+        "leaf",
+        "leaf",
+        "cube",
+        "cube",
+        "bomb",
+        "bomb",
+        "bicycle",
+        "bicycle",
+        "anchor",
+        "anchor",
+        "paper-plane-o",
+        "paper-plane-o",
+        "bolt",
+        "bolt"
     ],
     openedCards = [],
     $deck = $(".deck"),
@@ -22,13 +22,14 @@ let cards = [
     $scorePanel = $('#score-panel'),
     $moves = $('.moves'),
     $rating = $("i"),
-    delay = 500,
+    $restart = $(".restart"),
+    delay = 800,
     match = 0,
     moves = 0,
     amountOfCards = cards.length / 2,
     threeStars = amountOfCards + 2,
     twoStars = amountOfCards + 6,
-    oneStars = amountOfCards + 10;
+    oneStar = amountOfCards + 10;
 
 
 // Shuffle function
@@ -55,75 +56,94 @@ function newBoard() {
     moves = 0;
 
     for (let i = 0; i < cards.length; i++) {
-        $deck.append('<li class="card"><i class="' + cards[i] + '"></i></li>');
+        $deck.append($('<li class="card"><i class="fa fa-' + cards[i] + '"></i></li>'))
     }
-
+    cardClickListener();
 };
 
+// Set the rating and final score
 function setRating(moves) {
     let rating = 3;
     if (moves > threeStars && moves < twoStars) {
         $rating.eq(2).removeClass("fa-star").addClass("fa-star-o");
         rating = 2;
-    } else if (move > twoStars && move < oneStars) {
+    } else if (moves > twoStars && moves < oneStar) {
         $rating.eq(1).removeClass("fa-star").addClass("fa-star-o");
         rating = 1;
-    } else if (move > oneStars) {
+    } else if (moves > oneStar) {
         $rating.eq(0).removeClass("fa-star").addClass("fa-star-o");
         rating = 0;
     }
 
-    return { scrore: rating };
+    return { score: rating };
 };
 
+// Create the function to click cards
 let cardClickListener = function() {
-
-    $deck.find('.card:not(".match, .open")').bind('click', function() {
-        if ($('.show').length > 1) {
-            return true;
-        }
+    // click on cards that do not contain the class match or open
+    $deck.find('.card:not(".match, .open")').on("click", function() {
+        // only allow 2 cards with the class of show
+        if ($(".show").length > 1) { return true; }
         let $this = $(this);
-        let card = $this.context.innerHTML;
+        // Store the value of the specific card
+        let card = $this.children("i").attr("class");
 
-        $this.addClass('open show');
+        $this.addClass("open show");
         openedCards.push(card);
 
+        // Check if the opened card matches card in the array
         if (openedCards.length > 1) {
-            if (card.length === openedCards[0]) {
-                $deck.find('.open').addClass('match animated infinite rubberBand');
+            if (card === openedCards[0]) {
+                $deck.find(".open").addClass("match animated infinite rubberband");
                 setTimeout(function() {
-                    $deck.find('.match').removeClass('open show animated infinite rubberBand');
+                    $deck.find(".match").removeClass("open show animated infinite rubberband");
                 }, delay);
+                
+                // If cards match add 1 to match variable
                 match++;
+
             } else {
-                $deck.find('.open').addClass('notmatch animated infinite wobble');
+                $deck.find(".open").addClass("notmatch animated infinite wobble");
                 setTimeout(function() {
-                    $deck.find('.open').removeClass('animated infinite wobble');
+                    $deck.find(".open").removeClass("animated infinite wobble");
                 }, delay / 1.5);
+                setTimeout(function() {
+                    $deck.find(".open").removeClass("open show notmatch animated infinite wobble");
+                }, delay);
+
+                openedCards = [];
+                moves++;
+                setRating(moves);
             }
-            setTImeout(function() {
 
-                $deck.find('.open').removeClass('open show notmatch animated infinite wobble');
-            }, delay);
+            if (amountOfCards === match) {
+                setRating(moves);
+                let score = setRating(moves).score;
+                setTimeout(function() {
 
+                });
+            }
         }
 
-        openedCards = [];
-        moves++;
-        setRating(moves);
-        $moveNum.html(moves);
-
-        if (amountOfCards === match) {
-            setRating(moves);
-            var score = setRating(moves).score;
-            setTimeout(function() {
-                endGame(moves, score);
-            }, 500);
-        }
     });
+
 };
 
+function restartGame() {
+	$restart.on("click", function(){
+		newBoard();
+	});
+}
+restartGame();
+
+
 newBoard();
+
+
+
+
+
+
 
 
 
