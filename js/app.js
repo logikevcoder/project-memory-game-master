5da593ac035f381f587cb9853b7734f7642392e1
“Kevin <logikevcoder@gmail.com>
2017-10-13 11:23:13 -0500

    Added game modal for event of game won

--- js/app.js
+++ js/app.js
@@ -1,164 +1,184 @@
-(() => {
-	'use strict';
-
-let click1 = {},
-    click2 = {},
+let symbols = ['bicycle', 'bicycle', 'leaf', 'leaf', 'cube', 'cube', 'anchor', 'anchor', 'paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'bomb', 'bomb', 'diamond', 'diamond'],
     openedCards = [],
-    $deck = $(".deck"),
-    $card = (".card"),
-    $scorePanel = $('#score-panel'),
-    $moves = $('.moves'),
-    $rating = $("i"),
-    $restart = $(".restart"),
     match = 0,
     moves = 0,
-    pairs = 8;
+    clicks = 0,
+    $deck = $('.deck'),
+    $scorePanel = $('#score-panel'),
+    $moveNum = $('.moves'),
+    $ratingStars = $('i'),
+    $restart = $('.restart'),
+    timer;
 
 
+$('#winModal').show();
+$(".modal, .close").click(function() {
+    $('#winModal').hide();
+});
 
 
+const gameTimer = () => {
+    let startTime = new Date().getTime();
 
-class Card {
-    constructor(card, num) {
-        let cardID = card.id + '-' + num;
-        this.id = '#' + card.id + '-' + num;
-        this.image = card.image;
-        this.name = card.name;
-        this.html =
-            `<article class="card" id="${cardID}">
-        <div class="card-back">
-          <img src="images/${this.image}" class="card-image" >
-        </div>
-        <div class="card-front">
-          <img src="images/pokeball.png" class="card-image" >
-        </div>
-      </article>`;
-    }
-}
+    timer = setInterval(() => {
 
+        let now = new Date().getTime();
 
+        let elapsed = now - startTime;
 
-const makeCardArray = (data, level) => {
-    let cards = [];
-    // Add two of each card to the array
-    cards.forEach(function(card) {
-        cards.push(new Card(card, 1));
-        cards.push(new Card(card, 2));
-    });
+        let minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
 
-    return cards;
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        let currentTime = minutes + ':' + seconds;
+
+        $(".clock").text(currentTime);
+    }, 800);
+
 };
 
-// Shuffle function
-const shuffle = (array) => {
-    let currentIndex = array.length,
-        temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
+// Load the game on to the HTML page
+const boardInit = () => {
+    let cardImages = shuffle(symbols);
 
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+    match = 0;
+    moves = 0;
+    $moveNum.text('0');
+    
+    for (let i = 0; i < cardImages.length; i++) {
+        $deck.append($('<li class="card"><i class="fa fa-' + cardImages[i] + '"></i></li>'));
     }
 
+    clickCard();
+    $(".clock").text("0:00");
+
+};
+
+
+// Shuffle function to create random board every time
+const shuffle = (array) => {
+    let index = array.length,
+        temp, randomIndex;
+    while (0 !== index) {
+        randomIndex = Math.floor(Math.random() * index);
+        index -= 1;
+        temp = array[index];
+        array[index] = array[randomIndex];
+        array[randomIndex] = temp;
+    }
     return array;
 };
 
-const displayCards = (cardArray) => {
-	cardArray.forEach(function(card){
-		$("#game-board").append(card.html);
 
-		$(card.id).click(function() {
-			checkMatch(card);
-		});
-	});
-};
-
-// Create the function to click cards
-const checkCards = () => {
-    if (!clicked1.name) {
-        click1 = card;
-        $(card.id).addClass('show open');
-        return;
-
-    } else if (!click2.name && click1.id !== card.id) {
-        click2 = card;
-        $(card.id).addClass('show open');
-
-        moves++;
-        $(".moves").text(moves);
-
-    } else return;
-
-    if (click1.name === click2.name) {
-        foundMatch();
-    } else {
-        hideCards();
+// Create the rating function to change star ratings from 1-3
+const rating = (moves) => {
+    let score = 3;
+    if (moves <= 6) {
+        $ratingStars.eq(3).removeClass('fa-star').addClass('fa-star-o');
+        score = 3;
+    } else if (moves > 6 && moves <= 10) {
+        $ratingStars.eq(2).removeClass('fa-star').addClass('fa-star-o');
+        score = 2;
+    } else if (moves > 10) {
+        $ratingStars.eq(1).removeClass('fa-star').addClass('fa-star-o');
+        score = 1;
     }
 
+    return { score };
+
 };
 
 
-const matchedCards = () => {
-    matches++;
+//  Click event function to search for matched cards and modify them
+const clickCard = () => {
+    // Search the ul for cards that do not contain match or open classes
+    $deck.find('.card:not(".match, .open")').bind('click', function() {
+        clicks++;
+        clicks == 1 ? gameTimer() : '';
+        // only allow 2 cards with the class .show to be visible at once
+        if ($(".show").length > 1) { return true; };
+        let $this = $(this),
+            card = $this.find('i').prop('class');
+        // Check if the player has clicked the same card
+        if ($this.hasClass('open')) { return false; };
+        // Add classes open and show to the clicked card and push to array
+        $this.addClass('open show');
+        openedCards.push(card);
+        if (openedCards.length > 1) {
+            if (card === openedCards[0]) {
+                // call the found match function to check for matches
+                foundMatch();
+            } else {
+                // call the notMatch function if no match is found
+                notMatch();
+            }
 
-    if (matches === pairs) {
+            openedCards = [];
+            moves++;
+            rating(moves);
+            $moveNum.html(moves);
+        }
+
+
+        if (match === 8) {
+            rating(moves);
+            let score = rating(moves).score;
+            setTimeout(function() {
+
+            });
+        }
+    });
+};
+
+
+const foundMatch = () => {
+    $deck.find('.open').addClass('match');
+    setTimeout(function() {
+        $deck.find('.match').removeClass('open show');
+    }, 800);
+    match++;
+    if (match === 8) {
         gameOver();
     }
 };
 
 
-const hideCards = () => {
+const notMatch = () => {
+    $deck.find('.open').addClass('notmatch');
     setTimeout(function() {
-        $(click1.id).removeClass('open show');
-
-        // Reset click objects
-        click1 = {};
-        click2 = {};
+        $deck.find('.open').removeClass('.animated');
+    }, 800);
+    setTimeout(function() {
+        $deck.find('.open').removeClass('open show notmatch');
     }, 800);
 };
 
-const gameOver = () => {
 
+const gameOver = () => {
+    clearInterval(timer);
+
+    setTimeout(function (){
+        $('#winModal').show();
+    }, 500);
 };
 
-let cardArray = makeCardArray();
-
-displayCards(cardArray);
-
-})();
+ $('.modal-content').click(function(event) {
+    event.stopPropagation();
+  });
 
 
-//   	the array can be an array of objects instead of an array of strings.
-// so, to add an element, the syntax would be something like-
-// array.push({name:"card-name",position:1})
-// then to find if the array has a card with a name, 
-// you can use filter or reduce methods or a simple for 
-// loop will also work.
+// Function to reset the board
+$restart.bind('click', () => {
+    clicks = 0;
+    clearInterval(timer);
+    $('#winModal').hide();
+    $deck.empty();
+    boardInit();
+});
 
-// also, i would recommend that you make functions for each operations like- open card, close card, set card as matched,etc.
+boardInit();
 
-
-
-
-
-
-
-
-// /*
-//  * set up the event listener for a card. If a card is clicked:
-//  *  - display the card's symbol (put this functionality in another function that you call from this one)
-
-//  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-//  *  - if the list already has another card, check to see if the two cards match
-//  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you 
-//  call from this one)
-//  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in 
-//  another function that you call from this one)
-//  *    + increment the move counter and display it on the page (put this functionality in another function that you call 
-//  from this one)
-//  *    + if all cards have matched, display a message with the final score (put this functionality in another function that 
-//  you call from this one)
-//  */
--- js/test.js
+++ js/test.js
@@ -1,160 +1,53 @@
-let symbols = ['bicycle', 'bicycle', 'leaf', 'leaf', 'cube', 'cube', 'anchor', 'anchor', 'paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'bomb', 'bomb', 'diamond', 'diamond'],
-    openedCards = [],
-    match = 0,
-    moves = 0,
-    clicks = 0,
-    $deck = $('.deck'),
-    $scorePanel = $('#score-panel'),
-    $moveNum = $('.moves'),
-    $ratingStars = $('i'),
-    $restart = $('.restart'),
-    timer;
 
 
-const gameTimer = () => {
-    let startTime = new Date().getTime();
 
-    timer = setInterval(() => {
-
-        let now = new Date().getTime();
-
-        let elapsed = now - startTime;
-
-        let minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
-
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-
-        let currentTime = minutes + ':' + seconds;
-
-        $(".clock").text(currentTime);
-    }, 800);
-
-};
+  // Open start modal on load
 
 
-// Load the game on to the HTML page
-const boardInit = () => {
-    let cardImages = shuffle(symbols);
+  // Close modals when click outside modal
+  $('#winModal #close-win, #overlay').click(function() {
+    $('#winModal').hide();
+  });
 
-    match = 0;
+
+
+  $('.modal').click(function() {
+    $('.modal').hide();
+  });
+
+  $('.modal-content').click(function(event) {
+    event.stopPropagation();
+  });
+
+
+
+  // Restart game
+  $('#restart').click(function() {
+    $('#winModal').hide();
+    $('#startModal').show();
+  });
+
+  const startGame = (cards, level) => {
+
+    // reset game variables
+    gameStarted = false;
     moves = 0;
-    $moveNum.text('0');
-    $ratingStars.removeClass('fa-star-o').addClass('fa-star');
-    for (let i = 0; i < cardImages.length; i++) {
-        $deck.append($('<li class="card"><i class="fa fa-' + cardImages[i] + '"></i></li>'));
-    }
+    matches = 0;
+    setLevel(level);
 
-    clickCard();
-    $(".clock").text("0:00");
-};
+    // reset HTML
+    $('#game-board').empty();
 
+    $(".clock").text('0:00');
+    $("#moves").text('0');
+    $('#winModal').hide();
 
-// Shuffle function to create random board every time
-const shuffle = (array) => {
-    let index = array.length,
-        temp, randomIndex;
-    while (0 !== index) {
-        randomIndex = Math.floor(Math.random() * index);
-        index -= 1;
-        temp = array[index];
-        array[index] = array[randomIndex];
-        array[randomIndex] = temp;
-    }
-    return array;
-};
+    // Get cards and start the game!
+    let cardArray = makeCardArray(cardData, level);
 
+    shuffle(cardArray);
+    displayCards(cardArray);
+    displayStars(3);
+  };
 
-// Create the rating function to change star ratings from 1-3
-const rating = (moves) => {
-    let score = 3;
-    if (moves <= 6) {
-        $ratingStars.eq(3).removeClass('fa-star').addClass('fa-star-o');
-        score = 3;
-    } else if (moves > 6 && moves <= 10) {
-        $ratingStars.eq(2).removeClass('fa-star').addClass('fa-star-o');
-        score = 2;
-    } else if (moves > 10) {
-        $ratingStars.eq(1).removeClass('fa-star').addClass('fa-star-o');
-        score = 1;
-    }
-
-    return { score };
-};
-
-
-const foundMatch = () => {
-    $deck.find('.open').addClass('match');
-    setTimeout(function() {
-        $deck.find('.match').removeClass('open show');
-    }, 800);
-    match++;
-    console.log(match);
-};
-
-
-const notMatch = () => {
-    $deck.find('.open').addClass('notmatch');
-    setTimeout(function() {
-        $deck.find('.open').removeClass('.animated');
-    }, 800);
-    setTimeout(function() {
-        $deck.find('.open').removeClass('open show notmatch');
-    }, 800);
-};
-
-
-//  Click event function to search for matched cards and modify them
-const clickCard = () => {
-    // Search the ul for cards that do not contain match or open classes
-    $deck.find('.card:not(".match, .open")').bind('click', function() {
-        clicks++;
-        clicks == 1 ? gameTimer() : '';
-        // only allow 2 cards with the class .show to be visible at once
-        if ($(".show").length > 1) { return true; };
-        let $this = $(this),
-            card = $this.find('i').prop('class');
-        // Check if the player has clicked the same card
-        if ($this.hasClass('open')) { return false; };
-        // Add classes open and show to the clicked card and push to array
-        $this.addClass('open show');
-        openedCards.push(card);
-        if (openedCards.length > 1) {
-            if (card === openedCards[0]) {
-                // call the found match function to check for matches
-                foundMatch();
-            } else {
-                // call the notMatch function if no match is found
-                notMatch();
-            }
-
-            openedCards = [];
-            moves++;
-            rating(moves);
-            $moveNum.html(moves);
-        }
-
-
-        if (match === 8) {
-            rating(moves);
-            let score = rating(moves).score;
-            setTimeout(function() {
-
-            });
-        }
-    });
-};
-
-
-// Function to reset the board
-$restart.bind('click', () => {
-    clicks = 0;
-    clearInterval(timer);
-    $deck.empty();
-    boardInit();
-});
-
-
-boardInit();
+})();
